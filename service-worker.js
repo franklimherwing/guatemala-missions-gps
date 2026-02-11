5957a519de23d9520f71cdc1234f5b567644e1b0
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-02-10 21:17:40 -0500

    Implement service worker with caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,19 @@
+const cacheName = "missions-v1";
+const files = [
+ "index.html",
+ "styles.css",
+ "data.js"
+];
+
+self.addEventListener("install", e => {
+ e.waitUntil(
+ caches.open(cacheName).then(c => c.addAll(files))
+ );
+});
+
+self.addEventListener("fetch", e => {
+ e.respondWith(
+ caches.match(e.request).then(r => r || fetch(e.request))
+ );
+});
 
